ee1a2e344300f922972b3e686cb697d88e7e35bb
Haila Nasser <hailaalnasser94@gmail.com>
2024-05-21 18:30:49 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+let lengthRectangle = 10;
+let widthRectangle = 5;
+
+let theAreaRectangle = lengthRectangle * widthRectangle *2;
+console.log(theAreaRectangle);
+
+
+let stringOne = "thingOne";
+let stringTwo = "thingTwo";
+
+console.log(stringOne + stringTwo);
+console.log(`${stringOne}${stringTwo}`);
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+console.log(`The Intro to JavaScript course is ${adjective2}. James and Julia are so ${adjective1}. I cannot wait to work through the rest of this  content!"`)
+// question two 
+
+let FahrenheitTemperature = 10;
+
+// let FahrenheitTemperature = 10;/
+let resultDegree =  (FahrenheitTemperature - 32) / 1.8;
+console.log(resultDegree);
+
+// question three
+let hour = 6;
+let second = 3600;
+
+console.log(hour*second)
+
+
+
+
+let bill = 10.25;
+let tip = 3.14;
+let total = 7.15;
+
+let result = bill + tip + total;
+
+let firstName = "haila";
+let interest = "programming";
+let hobby = "reading";
+console.log( `the awesomeMessage  variable should have the correct format, as shown below:
+* "Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby} `)
+
